fb5f474e96bd810a476b7698546e046303cb5316
kalibre04 <82302756+kalibre04@users.noreply.github.com>
2021-04-12 14:01:26 +0800

    WTP

---
+++ app/Models/WildlifeTransportPermit.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WildlifeTransportPermit extends Model
+{
+    use HasFactory;
+}
