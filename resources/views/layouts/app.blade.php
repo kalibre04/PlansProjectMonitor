5656e0e6655f85c288eebad9a62920901c372ad5
kalibre04 <82302756+kalibre04@users.noreply.github.com>
2021-05-08 21:15:08 +0800

    defer script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         @livewireStyles
 
         <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="<?php echo asset('public/js/app.js') ?>" defer></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
