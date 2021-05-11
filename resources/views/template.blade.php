f5d150156e7fbbe08c866fd665b4055d5fa37bbd
kalibre04 <82302756+kalibre04@users.noreply.github.com>
2021-05-11 10:21:18 +0800

    Update template.blade.php

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -97,8 +97,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
-      <img src="<?php echo asset('public/AdminLTE/dist/img/AdminLTELogo.png')?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
+       <img src="<?php echo asset('/public/res/denr logo.png') ?>" alt="DENR Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">PPA Databank System</span>
     </a>
 
