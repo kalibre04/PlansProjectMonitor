6c517cfacf11edeae2dfd302c25e87e1b78ea123
kalibre04 <82302756+kalibre04@users.noreply.github.com>
2021-10-14 16:38:47 +0800

    Update PatrolTeamsController.php

--- app/Http/Controllers/PatrolTeamsController.php
+++ app/Http/Controllers/PatrolTeamsController.php
@@ -33,11 +33,11 @@ class PatrolTeamsController extends Controller
         $patrollers = LawinPatrollers::all()->pluck('fullname', 'id');
         $patrolteams = Patrolteams::selectRaw('id, CONCAT(team_sector, " - ", quarter," Quarter", " - ", year) as teamsector')->orderBy('teamsector', 'DESC')->pluck('teamsector', 'id');
 
-        $patrolssector = DB::table('LawinPatrollers')
-            ->join('PatrolAssignment', 'LawinPatroller.id', '=', 'PatrolAssignment.patroller_id')
-            ->join('PatrolTeams', 'PatrolAssignment.patrolteam_id', '=', 'PatrolTeams.id')
-            ->join('Offices', 'PatrolTeams.team_office', '=', 'office.id')
-            ->select('LawinPatrollers.fullname', 'PatrolTeams.team_sector', 'Offices.officename', 'PatrolTeams.quarter', 'PatrolTeams.year')->get();
+        $patrolssector = DB::table('lawin_patroller')
+            ->join('patrolassignment', 'lawin_patroller.id', '=', 'patrolassignment.patroller_id')
+            ->join('patrolteams', 'patrolassignment.patrolteam_id', '=', 'patrolteams.id')
+            ->join('office', 'patrolteams.team_office', '=', 'office.id')
+            ->select('lawin_patroller.fullname', 'patrolteams.team_sector', 'officeo.officename', 'patrolteams.quarter', 'patrolteams.year')->get();
             dd($patrolsector);
         return view('mes/lawin/patrolteams.index', compact('patrollers', 'patrolteams'));
     }
